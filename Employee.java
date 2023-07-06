d3f3692d5c13d34c5560aa5e398d0f0737839c2e
Ujwala0517 <138633941+Ujwala0517@users.noreply.github.com>
2023-07-06 06:52:50 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+public class Employee {
+   
+private int employeeID;
+   private String employeeName;
+
+public int getEmployeeID() {
+	return employeeID;
+}
+public void setEmployeeID(int employeeID) {
+	this.employeeID = employeeID;
+}
+public String getEmployeeName() {
+	return employeeName;
+}
+public void setEmployeeName(String employeeName) {
+	this.employeeName = employeeName;
+}
+public void setemployeeName( String employeeName) {
+
+	this.employeeName = employeeName;
+   }
+}
---
+++ EncapsulationExample.java
@@ -0,0 +1,9 @@
+package encapsulation;
+
+public class EncapsulationExample {
+   public static void main(String[] args) {
+	Employee employee = new Employee();
+	 employee.setEmployeeID(101);
+	 System.out.println(employee.getEmployeeID());
+}
+}
